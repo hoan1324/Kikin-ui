15cf68fb09db49eca1f06217da7283e43de824c0
hoan1324 <hoan3397@gmail.com>
2024-05-04 19:09:30 +0700

    no message

--- landing_page/src/Components/Layout/Components/Question/index.js
+++ landing_page/src/Components/Layout/Components/Question/index.js
@@ -2,37 +2,63 @@ import styles from "./QuestionStyle.module.scss"
 import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRectangleList, faCircleQuestion } from "@fortawesome/free-regular-svg-icons";
-import { faArrowRotateRight,faMinus } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRotateRight, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useRef, useState } from "react";
+
+import { question_detail } from "./question_detail";
 
 const cx = classNames.bind(styles)
-function Question() {
-    return (
-        <div className={cx("footer")}>
-            <h1>FAQS</h1>
-            <div className={cx("question-content")}>
-                <div className={cx("question-title")}>
-                    <div className={"questiontt"}>
-                        <FontAwesomeIcon icon={faRectangleList} />
-                        <span>FUNDING</span>
-                    </div>
-                    <div className={"questiontt"}>
-                        <FontAwesomeIcon icon={faArrowRotateRight} />
-                        <span>REPAYMENTS</span>
-                    </div>
-                    <div className={"questiontt"}>
-                        <FontAwesomeIcon icon={faCircleQuestion} />
-                        <span>GENERALS</span>
-                    </div>
 
-                </div>
-                <div className={cx("question-detail")}>
-                    <div className={"questiondt"}>
-                        <div className={"questiondt-child"}>
-                          <h2>How quickly can I access the funds?</h2>
-                          {/* <FontAwesomeIcon icon={faMinus} /> */}
-                          <FontAwesomeIcon icon={faMinus} />
-                          <p>Once you've completed a brief questionnaire about your business and connected your financial accounts, you'll have immediate access to a portion of your funds. The full amount will be available within 72 hours after our team manually checks your company details.</p>
+function Question() {
+    const [hidden, setHidden] = useState(true);
+    const [type,setType]=useState("")
+    const handleQuestion=(title)=>{
+        setHidden((pre) =>!pre)
+        setType(title)
+        console.log(child.current.getBoundingClientRect());
+        
+    }
+    let child=useRef()
+    let h2R=useRef();
+   
+    
+    return (
+        <div className={cx("questions")}>
+            <div className={cx("question")}>
+                <h1>FAQS</h1>
+                <div className={cx("question-content")}>
+                    <div className={cx("question-title")}>
+                        <div className={cx("enable","questiontt")}>
+                            <FontAwesomeIcon icon={faRectangleList} />
+                            <span>FUNDING</span>
                         </div>
+                        <div className={cx("questiontt")}>
+                            <FontAwesomeIcon icon={faArrowRotateRight} />
+                            <span>REPAYMENTS</span>
+                        </div>
+                        <div className={cx("questiontt")}>
+                            <FontAwesomeIcon icon={faCircleQuestion} />
+                            <span>GENERALS</span>
+                        </div>
+
+                    </div>
+                    <div className={cx("question-detail")}>
+                        {question_detail.map((questions) => (
+                            <div key={questions.Topic} className={cx("question-dt" ,+questions.Topic)}>
+                                {questions.detail.map((question) => (
+                                    <div ref={child} key={question.title} className={cx("question-child")}>
+                                        <div className={cx("title")} >
+                                            <h2 ref={h2R}>{question.title}</h2>
+                                            <FontAwesomeIcon icon={faMinus}
+                                                onClick={()=>handleQuestion(question.title)}
+                                            />
+                                        </div>
+                                        <p>{question.answer}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+
                     </div>
                 </div>
             </div>
@@ -40,4 +66,4 @@ function Question() {
     );
 }
 
-export { Question };
+export { Question};
